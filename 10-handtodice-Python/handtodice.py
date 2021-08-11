ba857f7eb2803ff389dc143024dbb23918ff6755
navinshady22 <navinshady22@gmail.com>
2021-08-11 20:08:56 +0530

    Solved!

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -4,7 +4,29 @@
 # assert(handToDice(214) == (2,1,4))
 # assert(handToDice(422) == (4,2,2))
 # Hint: You might find // and % useful here, and also getKthDigit().
+def getKthDigit(hand):
+	count = 0
+	while(hand>0):
+		rem = hand % 10
+		hand = hand // 10
+		count += 1
+	return count
 
 def handtodice(hand):
 	# your code goes here
+	# print(hand)
+	temp = hand
+	res = ()
+	numOfDigits = getKthDigit(hand)
+	while(numOfDigits>0):
+		temp = hand // 10**(numOfDigits - 1)
+		hand = hand % 10**(numOfDigits - 1)
+		res+= (temp, )
+		numOfDigits-=1
+		
+		# hand = hand // 10
+		# temp = hand
+	return res
 	pass
+
+# print(handtodice(int(input())))
