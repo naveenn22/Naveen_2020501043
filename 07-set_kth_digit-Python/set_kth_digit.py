875d611087e5c87bfe4d2970090942cbf1967e45
navinshady22 <navinshady22@gmail.com>
2021-08-12 21:55:32 +0530

    done!

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -4,8 +4,65 @@
 # the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
 # so the 0th digit is the rightmost digit. 
 
+# def getKthDigit(n, k):
+	
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	count = 1
+	res = 0 
+	flag=False
+	if(n>0):
+		while(n>0):
+			
+			temp = n % 10
+			n = n//10
+			if(count==k+1):
+				# print(res)
+				
+				res += d*(10**(count-1))
+				flag = True
+				# print(res)
+			else:
+				res += temp*(10**(count-1))
+				# print(res)
+			count+=1
+		# print(count)
+		# print(k)	
+		# # print("*********************")
+		# if(count>k):
+		# 	res+=d*(10**(count-1))
+		if(flag == False):
+			res += d*(10**(count-1))
+		return res
+	else:
+		count=1
+		num = abs(n)
+		s="1" + str(num)
+		n = int(s)
+		# print(s)
+		# print(100)
+		# print(n)
+		while(n>0):
+			
+			temp = n % 10
+			n = n//10
+			if(count==k+1):
+				# print(res)
+				
+				res += d*(10**(count-1))
+				# print(res)
+			else:
+				res += temp*(10**(count-1))
+				# print(res)
+			
+			count+=1
+		# print("*********************")
+		return int(-res)
+			
+print(fun_set_kth_digit(468, 0, 1,))		
+print(fun_set_kth_digit(468, 1, 1))	
+print(fun_set_kth_digit(468, 2, 1))	
+print(fun_set_kth_digit(468, 3, 1  ))	
+print(fun_set_kth_digit(-468, 3, 1))	
 
---
+++ 10-handtodice-Python/handtodice.py
@@ -0,0 +1,32 @@
+# Write the (very short) function handtodice(hand) that takes a hand, which is a 3-digit
+# integer, and returns 3 values, each of the 3 dice in the hand. For example:
+# assert(handToDice(123) == (1,2,3))
+# assert(handToDice(214) == (2,1,4))
+# assert(handToDice(422) == (4,2,2))
+# Hint: You might find // and % useful here, and also getKthDigit().
+def getKthDigit(hand):
+	count = 0
+	while(hand>0):
+		rem = hand % 10
+		hand = hand // 10
+		count += 1
+	return count
+
+def handtodice(hand):
+	# your code goes here
+	# print(hand)
+	temp = hand
+	res = ()
+	numOfDigits = getKthDigit(hand)
+	while(numOfDigits>0):
+		temp = hand // 10**(numOfDigits - 1)
+		hand = hand % 10**(numOfDigits - 1)
+		res+= (temp, )
+		numOfDigits-=1
+		
+		# hand = hand // 10
+		# temp = hand
+	return res
+	pass
+
+print(handtodice(int(input())))
