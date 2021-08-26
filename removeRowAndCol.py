a9b40be189ea5fcf82c60b668c76b4bd178f0645
navinshady22 <navinshady22@gmail.com>
2021-08-26 13:53:05 +0530

    done

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,16 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
+    # pass
+    L.pop(row)
+    # print(len(L))
+    for i in range(len(L)):
+        L[i].pop(col)
+    return (L)
+    # pass
+assert removeRowAndCol([ [ 2, 3, 4, 5],[ 8, 7, 6, 5],[ 0, 1, 2, 3] ],1,2) == [ [ 2, 3, 5],[ 0, 1, 3] ]
+assert removeRowAndCol([ [ 2, 3, 4, 5],[ 8, 7, 6, 5],[ 0, 1, 2, 3] ],2,2) == [ [ 2, 3, 5],[8,7,5,]]
+assert removeRowAndCol([ [ 2, 3, 4],[ 8, 7, 6],[ 0, 1, 2],[2,3,5] ],1,2) == [ [ 2, 3],[ 0, 1],[2,3]]
+
 
 # Write your own test cases.
