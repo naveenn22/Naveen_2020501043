95be1b45b5e0abdaad9a727f3b1f70cb2a11ed0d
navinshady22 <navinshady22@gmail.com>
2021-08-26 12:32:13 +0530

    done

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,13 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if(n==0):
+		return True
+	if(f==0 and  n!=0):
+		return False
+	elif(n%f==0):
+		return True
+	else:
+
+		# return False 
+		return False # replace with your solution
